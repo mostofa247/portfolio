1233cd5212ce29bb30c25a2b6de20044f7c9abd2
unknown <mdshourov5596@gmail.com>
2022-06-07 04:06:11 +0600

    update

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARfAiw8hY-uOM0b5WbAPrxgTh-gwjeR34",
+  authDomain: "mostofa-kamal-web.firebaseapp.com",
+  projectId: "mostofa-kamal-web",
+  storageBucket: "mostofa-kamal-web.appspot.com",
+  messagingSenderId: "1007380470724",
+  appId: "1:1007380470724:web:54e010a150f5af14ad2291",
+};
+
+// Initialize Firebase
+export default firebaseConfig;
